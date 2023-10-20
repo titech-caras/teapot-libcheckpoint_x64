31eba06b30a48be5d19905cbdc584eb91c2ed9fd
Toto Lin <toto@linux.com>
2023-10-20 15:35:43 +0900

    Refactor DIFT allocation

--- dift_support.c
+++ dift_support.c
@@ -4,24 +4,21 @@
 #include <string.h>
 #include <sys/mman.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <stdio.h>
 
+void *mmap_helper(void *base_addr, size_t len, int prot) {
+    int flags = MAP_ANONYMOUS | MAP_PRIVATE | MAP_FIXED_NOREPLACE | MAP_NORESERVE;
+    void *addr = mmap(base_addr, len, prot, flags, -1, 0);
+    if (addr != base_addr) {
+        fprintf(stderr, "Map address 0x%llx failed: %s\n", (unsigned long long)base_addr, strerror(errno));
+        abort();
+    }
+    return addr;
+}
+
 void map_dift_pages() {
-    void* high_tag = mmap((void *) 0x400000000000, 0x200000000000,
-                          PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE | MAP_FIXED_NOREPLACE | MAP_NORESERVE, -1, 0);
-    if ((long long) high_tag == -1)
-        fprintf(stderr, "High tag mapping error: %s\n", strerror(errno));
-    void* low_tag = mmap((void *) 0x200000000000, 0x7fff8000,
-                         PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE | MAP_FIXED_NOREPLACE, -1, 0);
-    if ((long long) low_tag == -1)
-        fprintf(stderr, "Low tag mapping error: %s\n", strerror(errno));
-    void *inaccessible1 = mmap((void *) 0x10007fff8000, 0x7fff8000,
-                               PROT_NONE, MAP_ANONYMOUS | MAP_PRIVATE | MAP_FIXED_NOREPLACE, -1, 0);
-    if ((long long) inaccessible1 == -1)
-        fprintf(stderr, "Inaccessible1 mapping error: %s\n", strerror(errno));
-    void *inaccessible2 = mmap((void *) 0x20007fff8000, 0x200000000000 - 0x7fff8000,
-                               PROT_NONE, MAP_ANONYMOUS | MAP_PRIVATE | MAP_FIXED_NOREPLACE, -1, 0);
-    if ((long long) inaccessible2 == -1)
-        fprintf(stderr, "Inaccessible2 mapping error: %s\n", strerror(errno));
+    mmap_helper((void *) 0x400000000000, 0x200000000000, PROT_READ | PROT_WRITE); // HighMem Tags
+    mmap_helper((void *) 0x200000000000, 0x7fff8000, PROT_READ | PROT_WRITE); // LowMem Tags
+    mmap_helper((void *) 0x10007fff8000, 0x100000000 - 0x7fff8000, PROT_NONE); // Gap
+    mmap_helper((void *) 0x20007fff8000, 0x200000000000 - 0x7fff8000, PROT_NONE); // Gap
 }
