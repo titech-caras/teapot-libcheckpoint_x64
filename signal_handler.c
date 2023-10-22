93616c07a5a56e960ec70e2deda16ea5b5a844ac
Toto Lin <toto@linux.com>
2023-10-23 01:52:01 +0900

    Add gadget addr output in signal handler

--- signal_handler.c
+++ signal_handler.c
@@ -12,10 +12,12 @@ extern void restore_checkpoint_SIGSEGV();
 
 void signal_handler(int sig, siginfo_t *info, void *ucontext) {
     if (checkpoint_cnt != 0) {
-        fprintf(stderr, "[NaHCO3], 11,\n");
-
+        uint64_t checkpoint_addr = checkpoint_metadata[checkpoint_cnt - 1].return_address;
         ucontext_t *uc = (ucontext_t *)ucontext;
         greg_t *rip = &uc->uc_mcontext.gregs[REG_RIP];
+
+        fprintf(stderr, "[NaHCO3], 11, 0x%lx, 0x%lx, 0, 0x%lx\n", *rip, (int64_t)info->si_addr, checkpoint_addr);
+
         *rip = (int64_t)&restore_checkpoint_SIGSEGV;
     } else {
         fprintf(stderr, "Signal caught outside simulation: %s\n", strsignal(sig));
