f15dc5ea7de905969c51b79e4a18046c0c67e298
Toto Lin <toto@linux.com>
2023-11-02 14:55:53 +0900

    Some dift wrappers for jsmn and yaml

--- dift_support.h
+++ dift_support.h
@@ -5,7 +5,38 @@
 #define TAG_ATTACKER 1
 #define TAG_SECRET 2
 
+typedef uint8_t dift_tag_t;
+
 // 0~15 = rax~r15, 16~47=zmm0~zmm31
-extern uint8_t dift_reg_tags[48];
+extern dift_tag_t dift_reg_tags[48];
+
+#define DIFT_REG_RAX 0
+#define DIFT_REG_RBX 1
+#define DIFT_REG_RCX 2
+#define DIFT_REG_RDX 3
+#define DIFT_REG_RSI 4
+#define DIFT_REG_RDI 5
+#define DIFT_REG_RSP 6
+#define DIFT_REG_RBP 7
+#define DIFT_REG_R8 8
+#define DIFT_REG_R9 9
+#define DIFT_REG_R10 10
+#define DIFT_REG_R11 11
+#define DIFT_REG_R12 12
+#define DIFT_REG_R13 13
+#define DIFT_REG_R14 14
+#define DIFT_REG_R15 15
+// TODO: xmm registers
+
+#define DIFT_ARG0 DIFT_REG_RDI
+#define DIFT_ARG1 DIFT_REG_RSI
+#define DIFT_ARG2 DIFT_REG_RDX
+#define DIFT_ARG3 DIFT_REG_RCX
+#define DIFT_ARG4 DIFT_REG_R8
+#define DIFT_ARG5 DIFT_REG_R9
+#define DIFT_RET DIFT_REG_RAX
+
+#define DIFT_MEM_ADDR(addr) ((dift_tag_t*)((size_t)addr ^ (1ULL << 45)))
+#define DIFT_MEM_TAG(addr) (*(DIFT_MEM_ADDR(addr)))
 
 void map_dift_pages();
---
+++ dift_wrappers.c
@@ -0,0 +1,49 @@
+#include "dift_support.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// TODO: eventually move this into an independent project and make the interface compatible with dfsan
+
+#define DIFT_WRAPPER(function_name, return_type, ...) return_type function_name##__dift_wrapper__(__VA_ARGS__)
+
+// Taint source: fread.
+DIFT_WRAPPER(fread, size_t, void *buffer, size_t size, size_t count, FILE *stream) {
+    size_t written_size = fread(buffer, size, count, stream);
+    if (written_size > 0) {
+        memset(DIFT_MEM_ADDR(buffer), TAG_ATTACKER, written_size);
+    }
+    return written_size;
+}
+
+DIFT_WRAPPER(strlen, size_t, const char *str) {
+    dift_reg_tags[DIFT_RET] = DIFT_MEM_TAG(str);
+    return strlen(str);
+}
+
+DIFT_WRAPPER(strdup, char*, const char *string) {
+    char *result = strdup(string);
+    memcpy(DIFT_MEM_ADDR(result), DIFT_MEM_ADDR(string), strlen(string));
+    return result;
+}
+
+DIFT_WRAPPER(memcpy, void*, void *dest, void *src, size_t count) {
+    memcpy(DIFT_MEM_ADDR(dest), DIFT_MEM_ADDR(src), count);
+    return memcpy(dest, src, count);
+}
+
+DIFT_WRAPPER(memmove, void*, void *dest, void *src, size_t count) {
+    memmove(DIFT_MEM_ADDR(dest), DIFT_MEM_ADDR(src), count);
+    return memmove(dest, src, count);
+}
+
+DIFT_WRAPPER(strcpy, char*, char *dest, const char *src) {
+    memcpy(DIFT_MEM_ADDR(dest), DIFT_MEM_ADDR(src), strlen(src));
+    return strcpy(dest, src);
+}
+
+DIFT_WRAPPER(memset, void*, void *dest, int ch, size_t count) {
+    memset(DIFT_MEM_ADDR(dest), dift_reg_tags[DIFT_ARG1], count);
+    return memset(dest, ch, count);
+}
